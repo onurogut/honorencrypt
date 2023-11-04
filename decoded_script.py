70940f0fbcd7e4612281af721dd8a98f65afd63c
Onur Öğüt <honorogut@gmail.com>
2023-11-04 23:42:24 +0300

    creating a structure
    desktop commit

---
+++ decoded_script.py
@@ -0,0 +1,13 @@
+# This is a simple Python script
+
+# Define a function
+def greet(name):
+    print("Hello, " + name + "!")
+
+# Main part of the script
+if __name__ == "__main__":
+    # Get user input
+    user_name = input("Enter your name: ")
+
+    # Call the greet function
+    greet(user_name)
